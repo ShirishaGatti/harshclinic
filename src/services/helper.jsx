792c4e5c098b7724006198a1ae7bca98e863267d
gatti13 <110365831+ShirishaGatti@users.noreply.github.com>
2025-06-02 17:15:57 +0530

    Update helper.jsx

--- src/services/helper.jsx
+++ src/services/helper.jsx
@@ -1,5 +1,10 @@
-import axios from "axios";
+ import axios from "axios";
 
+// export const myAxios = axios.create({
+//     baseURL: 'https://harsh-2onb.onrender.com', // Ensure backend server is running here
+// });
 export const myAxios = axios.create({
-    baseURL: 'https://harsh-2onb.onrender.com', // Ensure backend server is running here
+    baseURL: 'https://harsh-2onb.onrender.com',
+    withCredentials: true, // <-- very important
 });
+
